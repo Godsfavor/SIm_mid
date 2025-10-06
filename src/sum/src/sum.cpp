7cfda983a8b74a5c6c4870725436c682cb733a29
Godsfavor <godsfavour.gabriel@gmail.com>
2025-10-06 17:49:03 +0200

    Version 3: Changed topic type from string to number (Float64)

--- src/sum/src/sum.cpp
+++ src/sum/src/sum.cpp
@@ -1,19 +1,19 @@
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/float64.hpp"
 #include <iostream>
 
-void topic_callback(const std_msgs::msg::String::SharedPtr msg)
+void topic_callback(const std_msgs::msg::Float64::SharedPtr msg)
 {
-  std::cout << msg ->data <<std::endl;
+    std::cout << "Received number: " << msg->data << std::endl;
 }
 
 int main(int argc, char * argv[])
 {
-  rclcpp::init(argc, argv);
-  auto node = rclcpp::Node::make_shared("subcriber");
-  auto subscription = node->create_subscription<std_msgs::msg::String>("topic", 10, topic_callback);
-  rclcpp::spin(node);
-  rclcpp::shutdown();
-  return 0;
-
+    rclcpp::init(argc, argv);
+    auto node = rclcpp::Node::make_shared("subscriber");
+    auto subscription = node->create_subscription<std_msgs::msg::Float64>(
+        "topic", 10, topic_callback);
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }
